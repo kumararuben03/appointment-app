a8ab766d4eea667fa739da1bea1954ed06c3ce67
kumararuben03 <kumararuben0306@gmail.com>
2024-01-22 18:41:48 +0800

    Hotel App

---
+++ src/app/reservation-form/reservation-form.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
+
+@Component({
+  selector: 'app-reservation-form',
+  templateUrl: './reservation-form.component.html',
+  styleUrls: ['./reservation-form.component.css'],
+})
+export class ReservationFormComponent implements OnInit {
+  constructor(
+    private formBuilder: FormBuilder,
+    private reservationService: ReservationService
+  ) {}
+
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required],
+    });
+  }
+  reservationForm: FormGroup = new FormGroup({});
+
+  onSubmit() {
+    if (this.reservationForm.valid) {
+      let reservation: Reservation = this.reservationForm.value;
+      this.reservationService.addReservation(reservation);
+    }
+  }
+}
